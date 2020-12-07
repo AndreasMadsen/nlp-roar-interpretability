fix argparse issues with masking

comp550/dataset/roar_dataset.py
@@ -13,7 +13,7 @@ class ROARDataset(pl.LightningDataModule):
         model,
         base_dataset,
         k=1,
-        random_masking=False,
+        masking='top-k',
         batch_size=128,
         seed=0,
         num_workers=4,
@@ -33,7 +33,7 @@ class ROARDataset(pl.LightningDataModule):
         self._num_workers = num_workers
         self._batch_size = batch_size
         self._k = k
-        self._random_masking = random_masking
+        self._masking = masking
         self.tokenizer = base_dataset.tokenizer
 
     def _mask_batch(self, batch):
@@ -41,20 +41,22 @@ class ROARDataset(pl.LightningDataModule):
 
         for idx, (length, mask) in enumerate(zip(batch["length"], batch["mask"])):
             n_tokens = min(self._k, length - 2)
-            if self._random_masking:
+            if self._masking == 'random':
                 # Make sure only tokens that can be attended to are masked
                 attended_indices = torch.where(mask[:length])[0]
                 # Shuffle attended_indices
                 attended_indices = attended_indices[torch.randperm(len(attended_indices))]
                 # Select n_tokens
                 token_indices = attended_indices[:n_tokens]
-            else:
+            elif self._masking == 'top-k':
                 # Ensure that alpha is non-zero for attended tokens
                 #  and zero for non-attended tokens
                 alpha = torch.where(mask,
                                     alpha + torch.tensor(1e-6, dtype=alpha.dtype),
                                     torch.tensor(0, dtype=alpha.dtype))
                 _, token_indices = alpha[idx, :length].topk(k=n_tokens, dim=-1)
+            else:
+                raise ValueError(f'{self._masking} is not supported')
 
             # Mask tokens for ROAR
             batch["sentence"][idx, token_indices] = self.tokenizer.mask_token_id

experiments/stanford_nli_roar.py
@@ -24,11 +24,11 @@ parser.add_argument("--k",
                     default=1,
                     type=int,
                     help="The proportion of tokens to mask.")
-parser.add_argument("--random-masking",
+parser.add_argument("--masking",
                     action="store",
-                    default=False,
-                    type=bool,
-                    help="Whether to mask random tokens or not.")
+                    default='top-k',
+                    type=str,
+                    help="Use 'random' or 'top-k' masking.")
 parser.add_argument("--seed", action="store", default=0, type=int, help="Random seed")
 parser.add_argument("--num-workers",
                     action="store",
@@ -51,12 +51,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
     torch.set_num_threads(args.num_workers)
     seed_everything(args.seed)
-    experiment_id = f"snli_roar_s-{args.seed}_-k{args.k}_r-{int(args.random_masking)}"
+    experiment_id = f"snli_roar_s-{args.seed}_-k{args.k}_m-{args.masking[0]}"
 
     print('Running SNLI-ROAR experiment:')
     print(f' - k: {args.k}')
     print(f' - seed: {args.seed}')
-    print(f' - random-masking: {args.random_masking}')
+    print(f' - masking: {args.masking}')
 
     # Create ROAR dataset
     base_dataset = SNLIDataModule(
@@ -71,7 +71,7 @@ if __name__ == "__main__":
         model=base_model,
         base_dataset=base_dataset,
         k=args.k,
-        random_masking=args.random_masking,
+        masking=args.masking,
         seed=args.seed,
         num_workers=args.num_workers,
         batch_size=128,
@@ -116,5 +116,5 @@ if __name__ == "__main__":
     os.makedirs(f'{args.persistent_dir}/results', exist_ok=True)
     with open(f'{args.persistent_dir}/results/{experiment_id}.json', "w") as f:
         json.dump({"seed": args.seed, "dataset": "snli", "roar": True,
-                   "k": args.k, "random_masking": args.random_masking,
+                   "k": args.k, "masking": args.masking,
                    **results}, f)

experiments/stanford_sentiment_roar.py
@@ -24,11 +24,11 @@ parser.add_argument("--k",
                     default=1,
                     type=int,
                     help="The proportion of tokens to mask.")
-parser.add_argument("--random-masking",
+parser.add_argument("--masking",
                     action="store",
-                    default=False,
-                    type=bool,
-                    help="Whether to mask random tokens or not.")
+                    default='top-k',
+                    type=str,
+                    help="Use 'random' or 'top-k' masking.")
 parser.add_argument("--seed", action="store", default=0, type=int, help="Random seed")
 parser.add_argument("--num-workers",
                     action="store",
@@ -49,14 +49,14 @@ parser.add_argument("--use-gpu",
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    torch.set_num_threads(args.num_workers)
+    torch.set_num_threads(max(1, args.num_workers))
     pl.seed_everything(args.seed)
-    experiment_id = f"sst_roar_s-{args.seed}_k-{args.k}_r-{int(args.random_masking)}"
+    experiment_id = f"sst_roar_s-{args.seed}_k-{args.k}_m-{args.masking[0]}"
 
     print('Running SST-ROAR experiment:')
     print(f' - k: {args.k}')
     print(f' - seed: {args.seed}')
-    print(f' - random-masking: {args.random_masking}')
+    print(f' - masking: {args.masking}')
 
     # Create ROAR dataset
     base_dataset = StanfordSentimentDataset(
@@ -71,7 +71,7 @@ if __name__ == "__main__":
         model=base_model,
         base_dataset=base_dataset,
         k=args.k,
-        random_masking=args.random_masking,
+        masking=args.masking,
         seed=args.seed,
         num_workers=args.num_workers,
         batch_size=32,
@@ -114,5 +114,5 @@ if __name__ == "__main__":
     os.makedirs(f'{args.persistent_dir}/results', exist_ok=True)
     with open(f'{args.persistent_dir}/results/{experiment_id}.json', "w") as f:
         json.dump({"seed": args.seed, "dataset": "sst", "roar": True,
-                   "k": args.k, "random_masking": args.random_masking,
+                   "k": args.k, "masking": args.masking,
                    **results}, f)