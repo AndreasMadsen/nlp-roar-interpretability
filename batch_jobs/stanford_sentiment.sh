960ce31fa6bfc3dbe1a634409fb893408736e9ab
Andreas Madsen <amwebdk@gmail.com>
2020-12-08 20:17:59 +0100

    don't resubmit if results exists

--- batch_jobs/stanford_sentiment.sh
+++ batch_jobs/stanford_sentiment.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 for seed in {0..4}
 do
-    sbatch --time=0:15:0 --mem=12G -J sst_s-${seed} -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err"  ./python_job.sh \
-        experiments/stanford_sentiment.py \
-        --seed ${seed}
+    if [ ! -f $SCRATCH"/comp550/results/sst_s-${seed}.json" ]; then
+        echo sst_s-${seed}
+        sbatch --time=0:15:0 --mem=12G -J sst_s-${seed} -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err"  ./python_job.sh \
+            experiments/stanford_sentiment.py \
+            --seed ${seed}
+    fi
 done
