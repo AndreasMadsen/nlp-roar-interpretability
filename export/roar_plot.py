bb3d085015cd4fe016493642f11d780f77efd64c
Andreas Madsen <amwebdk@gmail.com>
2020-12-08 20:32:49 +0100

    add presistent-dir option to roar_plot.py

--- export/roar_plot.py
+++ export/roar_plot.py
@@ -1,6 +1,7 @@
 
 import glob
 import json
+import argparse
 import os.path as path
 
 import pandas as pd
@@ -8,7 +9,7 @@ import numpy as np
 import scipy
 import plotnine as p9
 
-thisdir = path.dirname(path.realpath(__file__))
+import os.path as path
 
 def ratio_confint(partial_df, column_name='f1_test'):
     """Implementes a ratio-confidence interval
@@ -32,39 +33,49 @@ def ratio_confint(partial_df, column_name='f1_test'):
         'n': len(x)
     })
 
-# Read JSON files into dataframe
-results = []
-for file in glob.glob(f'{thisdir}/../results/*.json'):
-    with open(file, 'r') as fp:
-        results.append(json.load(fp))
-df = pd.DataFrame(results)
+thisdir = path.dirname(path.realpath(__file__))
+parser = argparse.ArgumentParser()
+parser.add_argument('--persistent-dir',
+                    action='store',
+                    default=path.realpath(path.join(thisdir, '..')),
+                    type=str,
+                    help='Directory where all persistent data will be stored')
 
-# Dublicate k=0 for random and top-k masking strategies
-df = pd.concat([
-    df.fillna(value={'k': 0, 'masking': 'random'}),
-    df[df['masking'].isna()].fillna(value={'k': 0, 'masking': 'top-k'})])
+if __name__ == "__main__":
+    print('Starting ...')
+    args = parser.parse_args()
 
-# Compute confint and mean for each group
-df = df.groupby(['k', 'masking', 'dataset']).apply(ratio_confint)
-df.reset_index(inplace=True)
+    # Read JSON files into dataframe
+    results = []
+    for file in glob.glob(f'{args.persistent_dir}/results/*.json'):
+        with open(file, 'r') as fp:
+            results.append(json.load(fp))
+    df = pd.DataFrame(results)
 
-# Make the facet pretty
-df.replace({'dataset': {'sst': 'SST', 'snli': 'SNLI'}}, inplace=True)
+    # Dublicate k=0 for random and top-k masking strategies
+    df = pd.concat([
+        df.fillna(value={'k': 0, 'masking': 'random'}),
+        df[df['masking'].isna()].fillna(value={'k': 0, 'masking': 'top-k'})])
 
-# Generate result table
-print(df)
+    # Make the facet pretty
+    df.replace({'dataset': {'sst': 'SST', 'snli': 'SNLI'}}, inplace=True)
+    # Compute confint and mean for each group
+    df = df.groupby(['dataset', 'masking', 'k']).apply(ratio_confint)
 
-# Generate plot
-p = (p9.ggplot(df, p9.aes(x='k', color='masking'))
-    + p9.geom_line(p9.aes(y='mean'))
-    + p9.geom_point(p9.aes(y='mean'))
-    + p9.geom_errorbar(p9.aes(ymin='lower', ymax='upper'), width=0.4)
-    + p9.facet_grid('dataset ~ .')
-    + p9.labs(x='tokens removed', y='F1-Score', colour='')
-    + p9.ylim(0.5, 0.90)
-    + p9.theme(plot_margin=0,
-               legend_box = "vertical", legend_position="bottom",
-               text=p9.element_text(size=12)))
-# Save plot, the width is the \linewidth of a collumn in the LaTeX document
-p.save(f'{thisdir}/../plots/roar.pdf', width=3.03209, height=4, units='in')
-p.save(f'{thisdir}/../plots/roar.png', width=3.03209, height=4, units='in')
+    # Generate result table
+    print(df)
+
+    # Generate plot
+    p = (p9.ggplot(df.reset_index(), p9.aes(x='k', color='masking'))
+        + p9.geom_line(p9.aes(y='mean'))
+        + p9.geom_point(p9.aes(y='mean'))
+        + p9.geom_errorbar(p9.aes(ymin='lower', ymax='upper'), width=0.4)
+        + p9.facet_grid('dataset ~ .')
+        + p9.labs(x='tokens removed', y='F1-Score', colour='')
+        + p9.ylim(0.5, 0.90)
+        + p9.theme(plot_margin=0,
+                legend_box = "vertical", legend_position="bottom",
+                text=p9.element_text(size=12)))
+    # Save plot, the width is the \linewidth of a collumn in the LaTeX document
+    p.save(f'{thisdir}/../plots/roar.pdf', width=3.03209, height=4, units='in')
+    p.save(f'{thisdir}/../plots/roar.png', width=3.03209, height=4, units='in')
