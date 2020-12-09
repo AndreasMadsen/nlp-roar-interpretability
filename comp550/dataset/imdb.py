8ce4cb84fb3a207a9ec2a5f7ba6b43220e772174
Vaibhav <vaibhavadlakha95@gmail.com>
2020-12-09 15:11:28 +0530

    organized IMDB data loading code

--- comp550/dataset/imdb.py
+++ comp550/dataset/imdb.py
@@ -22,13 +22,14 @@ from .tokenizer import Tokenizer
 class IMDBTokenizer(Tokenizer):
     def __init__(self):
         '''
-        df is 10
+        Document frequency is 10
+        https://github.com/successar/AttentionExplanation/blob/master/preprocess/IMDB/IMDB.ipynb
         '''
         super().__init__()
         self.min_df = 10
 
     def tokenize(self, sentence):
-        return sentence
+        return sentence.split()
 
 
 class IMDBDataModule(pl.LightningDataModule):
@@ -64,6 +65,7 @@ class IMDBDataModule(pl.LightningDataModule):
         imdb_vocab_s3_url = 'https://s3.amazonaws.com/text-datasets/imdb_word_index.json'
 
         if not path.exists(self._cachedir + '/text-datasets/imdb_full.pkl'):
+            os.makedirs(self._cachedir + '/text-datasets', exist_ok=True)
             r = requests.get(imdb_data_s3_url)
             with open(self._cachedir + '/text-datasets/imdb_full.pkl', 'wb') as f:
                 f.write(r.content)
@@ -73,61 +75,78 @@ class IMDBDataModule(pl.LightningDataModule):
             with open(self._cachedir + '/text-datasets/imdb_word_index.json', 'wb') as f:
                 f.write(r.content)
 
+        if not path.exists(self._cachedir + '/text-datasets/imdb_full_text.pkl'):
+            
+            imdb_data = pickle.load(open(self._cachedir + '/text-datasets/imdb_full.pkl', 'rb'))
+            word_index = json.load(open(self._cachedir + '/text-datasets/imdb_word_index.json'))
+
+            train_set, test_set = imdb_data
+
+            idx_to_word_map = {idx:word for word, idx in word_index.items()}
+
+            '''
+            In the original implementation:
+             * Sentences with length greater than or equal to 400 are removed
+             * Only 20% of test set is used
+            https://github.com/successar/AttentionExplanation/blob/master/preprocess/IMDB/IMDB.ipynb 
+            '''
+            trainidx = [i for i, x in enumerate(train_set[0]) if len(x) < 400]
+            trainidx, devidx = train_test_split(trainidx, train_size=0.8)
+            testidx = [i for i, x in enumerate(test_set[0]) if len(x) < 400]
+            testidx, _ = train_test_split(testidx, train_size=0.2)
+
+            imdb_data = {}
+            for name, idxs, dataset in [('train', trainidx, train_set), ('val', devidx, train_set), ('test', testidx, test_set)]:
+                '''
+                Min length is 6 
+                https://github.com/successar/AttentionExplanation/blob/425a89a49a8b3bffc3f5e8338287e2ecd0cf1fa2/Trainers/DatasetBC.py#L108
+                '''
+                imdb_data[name] = [{
+                    'sentence': " ".join([idx_to_word_map[x] for x in dataset[0][idx]]),
+                    'label': dataset[1][idx]
+                } for idx in idxs if len(dataset[0][idx]) > 6]
+
+            with open(self._cachedir + '/text-datasets/imdb_full_text.pkl', 'wb') as fp:
+                pickle.dump(imdb_data, fp)
+
         torchtext.vocab.pretrained_aliases['fasttext.simple.300d'](
             cache=self._cachedir + '/embeddings')
 
         if not path.exists(self._cachedir + '/vocab/imdb.vocab'):
             os.makedirs(self._cachedir + '/vocab', exist_ok=True)
 
-            imdb_data = pickle.load(
-                open(self._cachedir + '/text-datasets/imdb_full.pkl', 'rb'))
-            initial_vocab = json.load(
-                open(self._cachedir + '/text-datasets/imdb_word_index.json'))
-            initial_vocab_inv = {idx: word for word,
-                                 idx in initial_vocab.items()}
-            train_set, test_set = imdb_data
+            imdb_data = pickle.load(open(self._cachedir + '/text-datasets/imdb_full_text.pkl', 'rb'))
+            self.tokenizer.from_iterable(x['sentence'] for x in imdb_data['train'])
 
-            trainidx = [i for i, x in enumerate(train_set[0]) if len(x) < 400]
-            trainidx, devidx = train_test_split(trainidx, train_size=0.8)
-
-            testidx = [i for i, x in enumerate(test_set[0]) if len(x) < 400]
-            testidx, _ = train_test_split(
-                testidx, train_size=0.2)
-
-            train_sentences = [[initial_vocab_inv[x]
-                                for x in train_set[0][i]] for i in trainidx]
-
-            self.tokenizer.from_iterable(
-                sentence for sentence in train_sentences)
             self.tokenizer.to_file(self._cachedir + '/vocab/imdb.vocab')
         else:
             self.tokenizer.from_file(self._cachedir + '/vocab/imdb.vocab')
 
         if not path.exists(self._cachedir + '/encoded/imdb.pkl'):
             os.makedirs(self._cachedir + '/encoded', exist_ok=True)
+            
+            imdb_data = pickle.load(open(self._cachedir + '/text-datasets/imdb_full_text.pkl', 'rb'))
 
             data = {}
-            for name, idxs, dataset in [('train', trainidx, train_set), ('val', devidx, train_set), ('test', testidx, test_set)]:
-                '''
-                Min length is 6 
-                https://github.com/successar/AttentionExplanation/blob/425a89a49a8b3bffc3f5e8338287e2ecd0cf1fa2/Trainers/DatasetBC.py#L108
-                '''
+            for name in ['train', 'val', 'test']:
+
+                dataset = imdb_data[name]
                 data[name] = [{
-                    'sentence': self.tokenizer.encode([initial_vocab_inv[x] for x in dataset[0][idx]]),
-                    'label': dataset[1][idx]
-                } for idx in idxs if len(dataset[0][idx]) > 6]
+                    'sentence': self.tokenizer.encode(instance['sentence']),
+                    'label': instance['label']
+                } for instance in dataset]
 
             with open(self._cachedir + '/encoded/imdb.pkl', 'wb') as fp:
                 pickle.dump(data, fp)
 
     def setup(self, stage=None):
         with open(self._cachedir + '/encoded/imdb.pkl', 'rb') as fp:
-            snli_dataset = pickle.load(fp)
+            dataset = pickle.load(fp)
         if stage == "fit":
-            self._train = self._process_data(snli_dataset['train'])
-            self._val = self._process_data(snli_dataset['val'])
+            self._train = self._process_data(dataset['train'])
+            self._val = self._process_data(dataset['val'])
         elif stage == 'test':
-            self._test = self._process_data(snli_dataset['test'])
+            self._test = self._process_data(dataset['test'])
         else:
             raise ValueError(f'unexpected setup stage: {stage}')
 
