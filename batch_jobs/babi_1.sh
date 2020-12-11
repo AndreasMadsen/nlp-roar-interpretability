83167730c56506606f57cfce2ed20606e8bb9fae
Nick Meade <nick-m@hotmail.ca>
2020-12-11 17:10:14 -0500

    Updated time limits for jobs

--- batch_jobs/babi_1.sh
+++ batch_jobs/babi_1.sh
@@ -3,7 +3,7 @@ for seed in {0..4}
 do
     if [ ! -f $SCRATCH"/comp550/results/babi_t-1_s-${seed}.json" ]; then
         echo babi_t-1_s-${seed}
-        sbatch --time=0:20:0 --mem=12G -J babi_t-1_s-${seed} -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err"  ./python_job.sh \
+        sbatch --time=0:15:0 --mem=12G -J babi_t-1_s-${seed} -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err"  ./python_job.sh \
             experiments/babi.py \
             --seed ${seed} \
             --task 1
--- batch_jobs/babi_1_roar.sh
+++ batch_jobs/babi_1_roar.sh
@@ -7,7 +7,7 @@ do
         do
             if [ ! -f $SCRATCH"/comp550/results/babi_t-1_roar_s-${seed}_k-${k}_m-${masking::1}.json" ]; then
                 echo babi_t-1_roar_s-${seed}_k-${k}_m-${masking::1}
-                sbatch --time=0:15:0 --mem=12G -J babi_t-1_roar_s-${seed}_k-${k}_m-${masking::1} -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err" ./python_job.sh \
+                sbatch --time=0:20:0 --mem=12G -J babi_t-1_roar_s-${seed}_k-${k}_m-${masking::1} -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err" ./python_job.sh \
                     experiments/babi_roar.py \
                     --seed ${seed} --k ${k} --masking ${masking} --task 1
             fi
