626b92db75d45d7628998f78608a12d472a8c9e5
Andreas Madsen <amwebdk@gmail.com>
2020-12-13 19:32:54 +0100

    use individual persentage scales

--- export/roar_plot.py
+++ export/roar_plot.py
@@ -59,7 +59,12 @@ if __name__ == "__main__":
         df[df['masking'].isna()].fillna(value={'k': 0, 'masking': 'top-k'})])
 
     # Make the facet pretty
-    df.replace({'dataset': {'sst': 'SST', 'snli': 'SNLI'}}, inplace=True)
+    df.replace({'dataset': {
+        'sst': 'SST',
+        'snli': 'SNLI',
+        'mimic-anemia': 'Anemia',
+        'mimic-diabetes': 'Diabetes'
+    }}, inplace=True)
     # Compute confint and mean for each group
     df = df.groupby(['dataset', 'masking', 'k']).apply(ratio_confint)
 
@@ -72,13 +77,13 @@ if __name__ == "__main__":
         + p9.geom_ribbon(p9.aes(ymin='lower', ymax='upper', fill='masking'), alpha=0.35)
         + p9.geom_line(p9.aes(y='mean', color='masking'))
         + p9.geom_point(p9.aes(y='mean', color='masking'))
-        + p9.facet_grid('dataset ~ .')
+        + p9.facet_grid('dataset ~ .', scales='free_y')
         + p9.labs(x='tokens removed', y='F1-Score', colour='')
-        + p9.ylim(0.4, 0.90)
+        + p9.scale_y_continuous(labels = lambda ticks: [f'{tick:.1%}' for tick in ticks])
         + p9.guides(fill=False)
         + p9.theme(plot_margin=0,
-                legend_box = "vertical", legend_position="bottom",
-                text=p9.element_text(size=12)))
+                   legend_box = "vertical", legend_position="bottom",
+                   text=p9.element_text(size=12)))
     # Save plot, the width is the \linewidth of a collumn in the LaTeX document
     os.makedirs(f'{args.persistent_dir}/plots', exist_ok=True)
     p.save(f'{args.persistent_dir}/plots/roar.pdf', width=3.03209, height=4, units='in')
