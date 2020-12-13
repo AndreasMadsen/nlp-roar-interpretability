fb7cb0b26a448ea56a5ffffaeaf5de3d3ea076f4
Andreas Madsen <amwebdk@gmail.com>
2020-12-13 12:14:41 +0100

    add batch jobs

--- batch_jobs/mimic_diabetes.sh
+++ batch_jobs/mimic_diabetes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for seed in {0..0}
+for seed in {0..4}
 do
     if [ ! -f $SCRATCH"/comp550/results/mimic-t-d_s-${seed}.json" ]; then
         echo mimic_t-d_s-${seed}
-        sbatch --time=2:50:0 --mem=24G -J mimic_t-d_s-${seed} -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err" ./python_job.sh \
+        sbatch --time=0:30:0 --mem=16G -J mimic_t-d_s-${seed} -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err" ./python_job.sh \
             experiments/mimic.py \
             --seed ${seed} \
             --subset diabetes
