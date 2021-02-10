8acb24f856edfb664f0f85b01f58a53a93738b9a
Andreas Madsen <amwebdk@gmail.com>
2021-02-10 18:31:36 +0100

    add extra time for gradient jobs

--- batch_jobs/babi_roar.sh
+++ batch_jobs/babi_roar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+declare -A time=( ["random"]="0:50:0" ["attention"]="0:50:0" ["gradient"]="1:50:0")
+
 for seed in {0..4}
 do
     for type in 1 2 3
@@ -9,7 +11,7 @@ do
             do
                 if [ ! -f $SCRATCH"/comp550/results/babi-${type}_s-${seed}_k-${k}_m-${importance_measure::1}_r-0.json" ]; then
                     echo babi-${type}_s-${seed}_k-${k}_m-${importance_measure::1}_r-0
-                    sbatch --time=0:50:0 --mem=24G \
+                    sbatch --time=${time[$importance_measure]} --mem=24G \
                         -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err" \
                         -J babi-${type}_s-${seed}_k-${k}_m-${importance_measure::1}_r-0 ./python_job.sh \
                         experiments/babi.py \
