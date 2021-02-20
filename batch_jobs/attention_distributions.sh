0d6132d952e9894b5647c22fa6b074cf3eeefeba
Nicholas Meade <nicholas.c.meade@gmail.com>
2021-02-20 12:50:57 -0500

    Fix job name

--- batch_jobs/attention_distributions.sh
+++ batch_jobs/attention_distributions.sh
@@ -2,6 +2,6 @@
 
 sbatch --time=0:30:0 --mem=8G \
     -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err" \
-    -J "attention_sparsity" ./python_job.sh \
+    -J "attention_distributions" ./python_job.sh \
     experiments/attention_distributions.py \
     --num-seeds 5
