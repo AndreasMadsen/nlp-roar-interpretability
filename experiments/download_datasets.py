27846914070243141308cf7f1ebe0e7eea7c405f
Nicholas Meade <nicholas.c.meade@gmail.com>
2021-03-02 13:18:19 -0500

    Update dataset names in dataset download script

--- experiments/download_datasets.py
+++ experiments/download_datasets.py
@@ -1,7 +1,7 @@
 import argparse
 import os.path as path
 
-from comp550.dataset import MimicDataset, SNLIDataModule, StanfordSentimentDataset, IMDBDataModule, BabiDataModule
+from comp550.dataset import MimicDataset, SNLIDataset, SSTDataset, IMDBDataset, BabiDataset
 
 thisdir = path.dirname(path.realpath(__file__))
 parser = argparse.ArgumentParser()
