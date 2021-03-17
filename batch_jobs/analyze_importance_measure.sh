0db2c00a874e9c21debbd39a82b74cccc4e4393a
Andreas Madsen <amwebdk@gmail.com>
2021-03-17 19:44:06 +0100

    update job allocation requirement

--- batch_jobs/analyze_importance_measure.sh
+++ batch_jobs/analyze_importance_measure.sh
@@ -4,7 +4,7 @@ for seed in {0..4}
 do
     for importance_measure in 'attention' 'gradient' 'integrated-gradient'
     do
-        sbatch --time=5:00:0 --mem=32G \
+        sbatch --time=2:00:0 --mem=16G \
             -o $SCRATCH"/comp550/logs/%x.%j.out" -e $SCRATCH"/comp550/logs/%x.%j.err" \
             -J "importance_s-${seed}_m-${importance_measure::1}" ./python_job.sh \
             experiments/analyze_importance_measure.py \
