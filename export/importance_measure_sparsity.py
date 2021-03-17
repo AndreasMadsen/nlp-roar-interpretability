00f59da1c71f810a939efea7127e0527ae389660
Andreas Madsen <amwebdk@gmail.com>
2021-03-17 21:50:03 +0100

    update export script

--- export/importance_measure_sparsity.py
+++ export/importance_measure_sparsity.py
@@ -26,7 +26,6 @@ parser.add_argument(
 )
 
 def _aggregate_importance(df):
-    # TODO: This assumes the importance is normalized
     importance_normalized = df['importance'] / np.sum(df['importance'])
     sorted_cumsum = np.cumsum(np.sort(importance_normalized))
     return pd.Series([
@@ -54,35 +53,38 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dataset_mapping = pd.DataFrame([
-        {"dataset": "sst", "print_name": "SST"},
-        {"dataset": "snli", "print_name": "SNLI"},
-        {"dataset": "imdb", "print_name": "IMDB"},
-        {"dataset": "babi-2", "print_name": "bAbI-2"},
-        {"dataset": "babi-1", "print_name": "bAbI-1"},
-        {"dataset": "babi-3", "print_name": "bAbI-3"},
-        {"dataset": "mimic-a", "print_name": "Diabetes"},
-        {"dataset": "mimic-d", "print_name": "Anemia"},
+        {"dataset": "sst", "dataset_pretty": "SST"},
+        {"dataset": "snli", "dataset_pretty": "SNLI"},
+        {"dataset": "imdb", "dataset_pretty": "IMDB"},
+        {"dataset": "babi-2", "dataset_pretty": "bAbI-2"},
+        {"dataset": "babi-1", "dataset_pretty": "bAbI-1"},
+        {"dataset": "babi-3", "dataset_pretty": "bAbI-3"},
+        {"dataset": "mimic-a", "dataset_pretty": "Diabetes"},
+        {"dataset": "mimic-d", "dataset_pretty": "Anemia"},
     ])
     importance_measure_mapping = {
+        'r': 'Random',
         'a': 'Attention',
         'g': 'Gradient',
-        'r': 'Random'
+        'i': 'Integrated Gradient'
     }
 
     # Read CSV files into a dataframe and progressively aggregate the data
     df_partials = []
     df_partials_keys = []
-    for file in tqdm(glob.glob(f'{args.persistent_dir}/results/attention/*.csv.gz'), desc='Parsing and summarzing CSVs'):
-        dataset, seed, measure = re.match(r'([0-9A-Za-z-]+)_s-(\d+)_m-([a-z])', path.basename(file)).groups()
+    for file in tqdm(sorted(glob.glob(f'{args.persistent_dir}/results/attention/*.csv.gz')), desc='Parsing and summarzing CSVs'):
+        filename = path.basename(file)
+        dataset, seed, measure = re.match(r'([0-9A-Za-z-]+)_s-(\d+)_m-([a-z])', filename).groups()
 
-        df_partial = _read_csv_tqdm(file, desc=f'Reading {dataset}_s-{seed}', leave=False, dtype={
+        df_partial = _read_csv_tqdm(file, desc=f'Reading {filename}', leave=False, dtype={
             'split': pd.CategoricalDtype(categories=["train", "val", "test"], ordered=True),
             'observation': np.int32,
             'index': np.int32,
-            'importance': np.float64
+            'token': np.int32,
+            'importance': np.float32
         })
 
-        tqdm.pandas(desc=f"Aggregating {dataset}_s-{seed}", leave=False)
+        tqdm.pandas(desc=f"Aggregating {filename}", leave=False)
         df_partial = df_partial.groupby(['split', 'observation'], observed=True).progress_apply(_aggregate_importance)
         df_partial = pd.melt(df_partial,
                              id_vars=['length'],
@@ -95,60 +97,54 @@ if __name__ == "__main__":
         df_partials_keys.append((dataset, int(seed), importance_measure_mapping[measure]))
 
     df = pd.concat(df_partials, keys=df_partials_keys, names=['dataset', 'seed', 'importance_measure'])
+    df_train = df.loc[pd.IndexSlice[:, :, :, 'train', :]]
 
     # Average over seeds
-    latex_df = df.loc[
-        pd.IndexSlice[:, :, 'train', :]
-    ].groupby(
-        ["dataset", "seed", "importance_measure", "percentage"]
-    ).agg({
-        'length': 'mean',
-        'absolute': 'mean',
-        'relative': 'mean'
-    }).groupby(
-        ["dataset", "importance_measure", "percentage"]
-    ).agg({
-        'length': partial(_compute_stats_and_format, percentage=False),
-        'absolute': partial(_compute_stats_and_format, percentage=False),
-        'relative': partial(_compute_stats_and_format, percentage=True)
-    }).reset_index(
-    ).merge(
-        dataset_mapping,
-        on="dataset"
-    ).pivot(
-        index=['print_name', 'importance_measure', 'length'],
-        columns=['percentage'],
-        values=['absolute', 'relative']
-    ).reset_index(
-        level='length'
+    latex_df = (
+        df_train
+            .groupby(["dataset", "seed", "importance_measure", "percentage"])
+            .agg({ 'length': 'mean', 'absolute': 'mean', 'relative': 'mean' })
+            .groupby(["dataset", "importance_measure", "percentage"])
+            .agg({
+                'length': partial(_compute_stats_and_format, percentage=False),
+                'absolute': partial(_compute_stats_and_format, percentage=False),
+                'relative': partial(_compute_stats_and_format, percentage=True)
+            })
+            .reset_index()
+            .merge(dataset_mapping, on="dataset")
+            .pivot(
+                index=['dataset_pretty', 'importance_measure', 'length'],
+                columns=['percentage'],
+                values=['absolute', 'relative']
+            )
+            .reset_index(level='length')
     )
     print(latex_df)
     os.makedirs(f"{args.persistent_dir}/tables", exist_ok=True)
-    latex_df.to_latex(f'{args.persistent_dir}/tables/attention_sparsity.tex', escape=False, index_names=False)
+    latex_df.to_latex(f'{args.persistent_dir}/tables/attention_sparsity.tex', escape=False, index_names=False, multirow=True)
 
     # Draw figure
-    plot_df = df.loc[
-        pd.IndexSlice[:, 0, 'train', :]
-    ].reset_index(
-    ).merge(
-        dataset_mapping,
-        on="dataset"
+    plot_df = (
+        df_train
+            .reset_index()
+            .merge(dataset_mapping, on="dataset")
     )
+    print(plot_df)
 
     p = (p9.ggplot(plot_df, p9.aes(x='absolute', fill='percentage'))
         + p9.geom_histogram(p9.aes(y=p9.after_stat('density')), position = "identity", alpha=0.5, bins=100)
-        + p9.facet_grid('print_name ~ importance_measure', scales='free')
+        + p9.facet_grid('dataset_pretty ~ importance_measure', scales='free')
         + p9.labs(x = 'number of tokens attended to'))
-    # Save plot, the 3.03209 is the \linewidth of a collumn in the LaTeX document
+    # Save plot, the 6.30045 is the \linewidth of \begin{figure*} in the LaTeX document
     os.makedirs(f'{args.persistent_dir}/plots', exist_ok=True)
-    p.save(f'{args.persistent_dir}/plots/attention_absolute.pdf', width=2*3.03209, height=7, units='in')
-    p.save(f'{args.persistent_dir}/plots/attention_absolute.png', width=2*3.03209, height=7, units='in')
+    p.save(f'{args.persistent_dir}/plots/importance_measure_absolute_sparsity.pdf', width=6.30045, height=7, units='in')
+    p.save(f'{args.persistent_dir}/plots/importance_measure_absolute_sparsity.png', width=6.30045, height=7, units='in')
 
     p = (p9.ggplot(plot_df, p9.aes(x='relative', fill='percentage'))
         + p9.geom_histogram(p9.aes(y=p9.after_stat('density')), position = "identity", alpha=0.5, bins=100)
-        + p9.facet_grid('print_name ~ importance_measure')
+        + p9.facet_grid('dataset_pretty ~ importance_measure')
         + p9.labs(x = 'relative number of tokens attended to'))
-    # Save plot, the 3.03209 is the \linewidth of a collumn in the LaTeX document
+    # Save plot, the 6.30045 is the \linewidth of \begin{figure*} in the LaTeX document
     os.makedirs(f'{args.persistent_dir}/plots', exist_ok=True)
-    p.save(f'{args.persistent_dir}/plots/attention_relative.pdf', width=2*3.03209, height=7, units='in')
-    p.save(f'{args.persistent_dir}/plots/attention_relative.png', width=2*3.03209, height=7, units='in')
+    p.save(f'{args.persistent_dir}/plots/importance_measure_relative_sparsity.pdf', width=6.30045, height=7, units='in')
+    p.save(f'{args.persistent_dir}/plots/importance_measure_relative_sparsity.png', width=6.30045, height=7, units='in')
