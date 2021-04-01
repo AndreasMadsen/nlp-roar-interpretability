652d7cabe7052f50b1bbef96b3659d6b9cbb33eb
Andreas Madsen <amwebdk@gmail.com>
2021-04-01 10:41:43 +0200

    measure cross-entropy on val and test dataset (#25)
    * measure cross-entropy on val and test dataset
    
    * remove ce from progress-bar

--- comp550/model/single_sequence_to_class.py
+++ comp550/model/single_sequence_to_class.py
@@ -1,11 +1,13 @@
 from typing import Tuple, Optional
 
+import warnings
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
 
+from ._total_ce_loss import TotalCrossEntropyLoss
 from ..dataset import SequenceBatch
 
 class _Encoder(nn.Module):
@@ -91,11 +93,20 @@ class SingleSequenceToClass(pl.LightningModule):
         self.ce_loss = nn.CrossEntropyLoss()
 
         self.val_metric_acc = pl.metrics.Accuracy(compute_on_step=False)
-        self.val_metric_auroc = pl.metrics.AUROC(num_classes=num_of_classes, compute_on_step=False)
         self.val_metric_f1 = pl.metrics.F1(num_classes=num_of_classes, average='macro', compute_on_step=False)
+        self.val_metric_ce = TotalCrossEntropyLoss()
+
         self.test_metric_acc = pl.metrics.Accuracy(compute_on_step=False)
-        self.test_metric_auroc = pl.metrics.AUROC(num_classes=num_of_classes, compute_on_step=False)
         self.test_metric_f1 = pl.metrics.F1(num_classes=num_of_classes, average='macro', compute_on_step=False)
+        self.test_metric_ce = TotalCrossEntropyLoss()
+
+        with warnings.catch_warnings():
+            # Ignore this warning:
+            # Metric `AUROC` will save all targets and predictions in buffer.
+            # For large datasets this may lead to large memory footprint.
+            warnings.filterwarnings("ignore", category=UserWarning)
+            self.val_metric_auroc = pl.metrics.AUROC(num_classes=num_of_classes, compute_on_step=False)
+            self.test_metric_auroc = pl.metrics.AUROC(num_classes=num_of_classes, compute_on_step=False)
 
     @property
     def embedding_matrix(self):
@@ -124,6 +135,7 @@ class SingleSequenceToClass(pl.LightningModule):
         self.val_metric_acc.update(predict_label, batch.label)
         self.val_metric_auroc.update(predict_prop, batch.label)
         self.val_metric_f1.update(predict_label, batch.label)
+        self.val_metric_ce.update(y, batch.label)
 
     def test_step(self, batch: SequenceBatch, batch_nb):
         y, _, _ = self.forward(batch)
@@ -132,22 +144,27 @@ class SingleSequenceToClass(pl.LightningModule):
         self.test_metric_acc.update(predict_label, batch.label)
         self.test_metric_auroc.update(predict_prop, batch.label)
         self.test_metric_f1.update(predict_label, batch.label)
+        self.test_metric_ce.update(y, batch.label)
 
     def validation_epoch_end(self, outputs):
-        self.log(f'acc_val', self.val_metric_acc.compute(), on_epoch=True)
-        self.log(f'auroc_val', self.val_metric_auroc.compute(), on_epoch=True, prog_bar=True)
-        self.log(f'f1_val', self.val_metric_f1.compute(), on_epoch=True, prog_bar=True)
+        self.log('acc_val', self.val_metric_acc.compute(), on_epoch=True)
+        self.log('auroc_val', self.val_metric_auroc.compute(), on_epoch=True, prog_bar=True)
+        self.log('f1_val', self.val_metric_f1.compute(), on_epoch=True, prog_bar=True)
+        self.log('ce_val', self.val_metric_ce.compute(), on_epoch=True)
         self.val_metric_acc.reset()
         self.val_metric_auroc.reset()
         self.val_metric_f1.reset()
+        self.val_metric_ce.reset()
 
     def test_epoch_end(self, outputs):
-        self.log(f'acc_test', self.test_metric_acc.compute(), on_epoch=True)
-        self.log(f'auroc_test', self.test_metric_auroc.compute(), on_epoch=True, prog_bar=True)
-        self.log(f'f1_test', self.test_metric_f1.compute(), on_epoch=True, prog_bar=True)
+        self.log('acc_test', self.test_metric_acc.compute(), on_epoch=True)
+        self.log('auroc_test', self.test_metric_auroc.compute(), on_epoch=True, prog_bar=True)
+        self.log('f1_test', self.test_metric_f1.compute(), on_epoch=True, prog_bar=True)
+        self.log('ce_test', self.test_metric_ce.compute(), on_epoch=True)
         self.test_metric_acc.reset()
         self.test_metric_auroc.reset()
         self.test_metric_f1.reset()
+        self.test_metric_ce.reset()
 
     def configure_optimizers(self):
         return torch.optim.Adam([
