741c40132565dfd773f80bee2fe851270e332874
Andreas Madsen <amwebdk@gmail.com>
2021-05-24 15:39:09 +0200

    add backup script

---
+++ pack_scratch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=rrg-bengioy-ad
+#SBATCH -J pack-scratch
+#SBATCH -o /home/anmadc/logs/%x.%j.out
+#SBATCH -e /home/anmadc/logs/%x.%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=2G
+#SBATCH --time=16:00:00
+
+module load python/3.8.2
+
+# Create environment
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+pip install --no-index tqdm
+
+# Suggestion from https://docs.computecanada.ca/wiki/Handling_large_collections_of_files
+tar -vc --use-compress-program="pigz -p 4" -f $SCRATCH/comp550.tar.gz $SCRATCH/comp550/ | tqdm --total $(find $SCRATCH/comp550/ | wc -l) > /dev/null
