c95e75fec993a25a12d1f0af138d6fd3e7da7aa0
Andreas Madsen <amwebdk@gmail.com>
2021-08-06 19:56:28 +0200

    update dependencies to match compute-canada

--- python_beluga_cpu_job.sh
+++ python_beluga_cpu_job.sh
@@ -5,14 +5,14 @@
 #SBATCH --time=2:00:00
 
 # Load modules
-module load python/3.8.2
+module load python/3.8.10
 
 # Create environment
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 
 # Install dependencies
-python -m pip install --no-index --no-deps $HOME/python_wheels/en_core_web_sm-3.0.0-py3-none-any.whl
+python -m pip install --no-index --no-deps $HOME/python_wheels/en_core_web_sm-3.1.0-py3-none-any.whl
 python -m pip install --no-index 'chardet<4.0,>=2.0' 'click<7.2.0,>=7.1.1'
 
 # Install comp550
