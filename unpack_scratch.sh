7ddaca1f75fcf8a951e3d1412d3a3e6ec87e1289
Andreas Madsen <amwebdk@gmail.com>
2021-08-24 14:09:37 +0200

    fix unpack_scratch.sh

--- unpack_scratch.sh
+++ unpack_scratch.sh
@@ -4,7 +4,7 @@
 #SBATCH -o /home/anmadc/logs/%x.%j.out
 #SBATCH -e /home/anmadc/logs/%x.%j.err
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=2G
-#SBATCH --time=16:00:00
+#SBATCH --mem=10G
+#SBATCH --time=20:00:00
 
-tar -xvf --touch $SCRATCH/comp550.tar.gz -C /
+tar -mxvf $SCRATCH/comp550.tar.gz -C /
